900feaf81b779edf02ebf26267f8fbdf90835883
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-08-13 17:17:00 +1000

    added the table

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -1,17 +1,26 @@
 // from data.js
 var tableData = data;
-var table = d3.select("tbody");
+var tbody = d3.select("tbody");
 
-// Using the UFO dataset provided in the form of an array of JavaScript objects,
-//write code that appends a table to your web page and then adds 
-// new rows of data for each UFO sighting.
+// Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a 
+// table to your web page and then adds  new rows of data for each UFO sighting.
+// Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
 
-{//                             table id="ufo-table" class="table table-striped">
-
-
-// Make sure you have a column for date/time, city, state, country, shape, and 
-// comment at the very least.
-
+tableData.forEach(function(ufo_sight) {
+    console.log(ufo_sight);
+    var row = tbody.append("tr");
+    Object.entries(ufo_sight).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
 
 // Use a date form in your HTML document and write JavaScript code that will listen for events and search
 //  through the date/time column to find rows that match user input.
+
+
+
+
