d552f77ae493ee2c3b8c138d06fc6594186682cf
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-08-13 19:40:29 +1000

    update

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -21,6 +21,31 @@ tableData.forEach(function(ufo_sight) {
 // Use a date form in your HTML document and write JavaScript code that will listen for events and search
 //  through the date/time column to find rows that match user input.
 
+var button = d3.select("#filter-btn");
 
+// Select the filter
+var form = d3.select("#filters");
 
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
 
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+};
